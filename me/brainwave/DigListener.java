f8fd1a6a24a7ae4a4b73a5f9dce7761573510423
Brainwave314 <thegodcomplex.mc@gmail.com>
2017-03-30 16:49:46 -0400

    Create DigListener.java

---
+++ me/brainwave/DigListener.java
@@ -0,0 +1,29 @@
+package me.brainwave;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerToggleSneakEvent;
+
+import com.projectkorra.projectkorra.BendingPlayer;
+import com.projectkorra.projectkorra.ability.CoreAbility;
+
+public class DigListener implements Listener{
+
+	@EventHandler
+	public void onSneak(PlayerToggleSneakEvent event) {
+		Player player = event.getPlayer();
+		if (player.isSneaking()) {
+			return;
+		}
+		
+		BendingPlayer bPlayer = BendingPlayer.getBendingPlayer(player);
+		if (bPlayer == null) {
+			return;
+		}
+		
+		if (bPlayer.canBend(CoreAbility.getAbility("Dig"))) {
+			new Dig(player);
+		}
+	}
+}
